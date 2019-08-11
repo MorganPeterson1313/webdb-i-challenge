c8d18d344a16b9f10c5e2f9a5ed22675448bd645
Morgan Peterson <47166767+MorganPeterson1313@users.noreply.github.com>
2019-08-10 22:48:28 -0400

    mvp complete

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const db = require('./data/dbConfig.js');
 
+const accountsRouter = require('./accounts-router')
+
 const server = express();
 
 server.use(express.json());
-
+server.use('/api/accounts' , accountsRouter)
 
 
 
